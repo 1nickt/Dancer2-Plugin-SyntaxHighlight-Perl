f19a739e058df9814afbfe3ce412c02f784366e9
Nick Tonkin <1nickt@users.noreply.github.com>
2017-11-15 15:45:03 -0500

    Update Changes and $VERSION

--- lib/Dancer2/Plugin/SyntaxHighlight/Perl.pm
+++ lib/Dancer2/Plugin/SyntaxHighlight/Perl.pm
@@ -1,6 +1,6 @@
 package Dancer2::Plugin::SyntaxHighlight::Perl;
 
-our $VERSION = '0.0008';
+our $VERSION = '0.0009';
 
 use strict; use warnings;
 use Data::Dumper; $Data::Dumper::Indent = $Data::Dumper::Sortkeys = 1;
